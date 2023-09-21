ccd8a3160a010f90dd1e10589b1adeb424a67aa7
Wandrest94 <ngangaernest9@gmail.com>
2023-09-21 08:29:15 -0700

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_list - normally prints the elements of  linked list
+ * @h: is pointer to the list_t list to be printed
+ *
+ * Return: value or  number of nodes to be printed
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,10 @@
 
 void first(void) __attribute__ ((constructor));
 
+/**
+ * first - function to print a sentence before main
+ * function is executed
+ */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
